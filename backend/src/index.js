45f1eb16330efaa5e9746f0c12d62bd8b7b6432c
Deepak Arora <arroadeepak0817@gmail.com>
2024-01-27 18:03:26 +0530

    corrected cors address

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors({
-    origin:"http://localhost:7000",
+    origin:"http://localhost:5173",
     credentials : true,
 }));
 

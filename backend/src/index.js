5a98ba81de272b787a52c693b3fc09e6971b5bc6
Deepak Arora <arroadeepak0817@gmail.com>
2024-01-27 19:03:41 +0530

    cors revert

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cors({
-    // origin:"https://holiday-harbor.onrender.com",
+    origin:process.env.FRONTEND-URL,
     credentials : true,
 }));
 
